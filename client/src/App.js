a91d3a1ab43d2d2b105f4f897ebf3f2b6a051bd3
namita.shenai@gmail.com <namita.shenai@gmail.com>
2022-01-03 22:39:51 -0800

    project list json file and update theapp.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+//import all pages & components 
+import Landing from "./pages/Landing"; 
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact"; 
+import About from "./pages/AboutMe"; 
+import Project from "./pages/Project"; 
+// import NavBar from "./components/Navbar"; 
+// import Footer from "./components/Footer"; 
 function App() {
   return (
     <div className="App">
-      <h1>Homepage</h1>
+     
     </div>
   );
 }
