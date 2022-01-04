f9b54f1bcbb645cdc88e5dfd29cc805cf0d77832
namita.shenai@gmail.com <namita.shenai@gmail.com>
2022-01-03 22:58:23 -0800

    Updated About Me

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -8,7 +8,7 @@ function About() {
             <div className="container pt-2">
                 <div className="row pt-5">
                 <div className="col-lg-6 mx-auto pt-5">
-                After prioritizing family and motherhood for few years I am back to doing what I love the most, coding and building applications.<br/> Completed <b>Full Stack Bootcamp</b> program at UC Berkeley.<br/> I am proficient at CSS, HTML, Javascript, Node.js, jQuery, Bootstrap, MySQL, MongoDB, Express.js and React.js.<br/> I have previously worked in the technology industry for almost 7 years as a developer on Microsoft technologies such as ASP.Net[c#], SharePoint, SQL, SharePoint designer.<br />
+                {/* After prioritizing family and motherhood for few years I am back to doing what I love the most, coding and building applications.<br/> Completed <b>Full Stack Bootcamp</b> program at UC Berkeley.<br/> I am proficient at CSS, HTML, Javascript, Node.js, jQuery, Bootstrap, MySQL, MongoDB, Express.js and React.js.<br/> I have previously worked in the technology industry for almost 7 years as a developer on Microsoft technologies such as ASP.Net[c#], SharePoint, SQL, SharePoint designer.<br /> */}
                 
                 </div>
                 <div className="col-lg-6 mx-auto pt-5">
